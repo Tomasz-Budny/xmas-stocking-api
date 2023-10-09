0a053feb7889398d255582e4f1bdbe8d42e7c375
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-09 11:32:43 +0200

    Add SmtpService with implementation

---
+++ xmas-stocking.Api/Services/ISmtpService.cs
@@ -0,0 +1,9 @@
+﻿using xmas_stocking.Api.Models;
+
+namespace xmas_stocking.Api.Services
+{
+    public interface ISmtpService
+    {
+        Task SendEmailsToGiftPresenters(IEnumerable<GiftPresenter> giftPresenters);
+    }
+}
---
+++ xmas-stocking.Api/Services/SmtpService.cs
@@ -0,0 +1,12 @@
+﻿using xmas_stocking.Api.Models;
+
+namespace xmas_stocking.Api.Services
+{
+    public class SmtpService : ISmtpService
+    {
+        public Task SendEmailsToGiftPresenters(IEnumerable<GiftPresenter> giftPresenters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
