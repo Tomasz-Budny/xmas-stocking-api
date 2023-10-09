6b537c4bb861a1631b2f0fec6b0b921a529f5d13
Tomasz-Budny <s208246@sggw.edu.pl>
2023-10-09 13:24:10 +0200

    Enable swagger annotations

--- xmas-stocking.Api/Program.cs
+++ xmas-stocking.Api/Program.cs
@@ -11,7 +11,10 @@ ConfigurationManager configuration = builder.Configuration;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+});
 
 builder.Services.AddScoped<ErrorHandlerMiddleware>();
 builder.Services.AddScoped<IDrawnService, DrawnService>();
